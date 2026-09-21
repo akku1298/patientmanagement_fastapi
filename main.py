e3bdcc3f3e41ab402f48bc7551443b9d11467cf9
Aman Srivastav <amansrivastav.106004@gmail.com>
2026-09-21 16:40:21 +0530

    Allow Vercel CORS

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:5173"],
+  allow_origins=[
+    "http://localhost:5173",
+    "https://patientmanagement-fastapi.vercel.app",
+  ],
   allow_methods=["*"],
   allow_headers=["*"],
 )
